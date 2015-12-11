e8b50cb2bf934bd2d74cc9b4f65ca4ad36eaf28a
César Eduardo Ortíz Salazar <pkgears@gmail.com>
2015-12-11 14:06:45 -0600

    Migraciones y modelos

---
+++ Blog/database/migrations/2015_12_09_223342_add_articles_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('content');
+            $table->integer('user_id')->unsigned();
+            $table->integer('categorie_id')->unsigned();
+            
+            //Llaves foraneas
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('categorie_id')->references('id')->on('categories')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('articles');
+    }
+}
